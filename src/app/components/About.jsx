44464ad5c82da9ef357dc4e4600f9dc09974e5ad
Dennis Avalos <dennisavalos95@gmail.com>
2024-04-30 14:04:47 -0500

    added skills and certifications tab

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2  text-[#ADB7BE]">
+        <li>CS50x: Introduction to Computer Science - Harvard University</li>
+        <li>Responsive Web Design - freeCodeCamp</li>
+        <li>Front End Development Libraries - freeCodeCamp</li>
+        <li>Back End Development and APIs - In Progress</li>
+        <li>JavaScript Algorithms and Data Structures - In Progress</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Hobbies",
+    id: "hobbies",
+    content: (
+      <ul className="list-disc pl-2  text-[#ADB7BE]">
+        <li>Powerlifting</li>
+        <li>Watching F1</li>
+        <li>Building Legos</li>
+        <li>Reading Sci-Fi novels</li>
+        <li>Traveling</li>
+      </ul>
+    ),
+  },
+];
 
 const About = () => {
+  const [tab, setTab] = useState("certifications");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
     <section id="#about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
@@ -12,7 +53,7 @@ const About = () => {
           height={500}
           className="rounded-lg"
         />
-        <div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className=" text-[#ADB7BE] text-base lg:text-lg">
             My name is Dennis Avalos, a passionate full-stack developer with a
@@ -24,17 +65,27 @@ const About = () => {
             committed to delivering projects on time and exceeding client
             expectations. I believe in the importance of clear communication,
             collaboration, and adaptability to ensure successful outcomes in
-            every project I undertake. When I&apos;m not coding, you can find me
-            exploring new technologies, browsing forums/articles to further hone
-            my skills, as well as going to the gym and watching F1 races on the
-            weekend. I am excited to showcase my work and accomplishments in
-            this portfolio and look forward to the opportunity to collaborate on
-            future projects. Thank you for visiting!
+            every project I undertake. I am excited to showcase my work and
+            accomplishments in this portfolio and look forward to the
+            opportunity to join your team. Thank you for visiting!
           </p>
-          {/* <div className="flex flex-row mt-8">
-            <span className="mr-3 font-semibold">Education</span>
-            <span>Hobbies</span>
-          </div> */}
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certfications
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("hobbies")}
+              active={tab === "hobbies"}
+            >
+              Hobbies
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
     </section>
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,19 +4,17 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
-      <div className="container p-12 px-12 items-center flex justify-between ">
+      <div className="container items-center flex justify-between ">
         <span>
           <Image
             src="/images/logo.png"
             alt="hero image"
             width={50}
             height={50}
-            className="ml-40"
           />
         </span>
-        <p className="text-slate-600">
-          All rights reserved/Created by. Dennis Avalos
-        </p>
+        <p className="text-slate-600">Created by: Dennis Avalos</p>
+        <p className="text-slate-600">All rights reserved</p>
       </div>
     </footer>
   );
